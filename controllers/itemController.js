27be19a24053058962ddfdf0cb7ebace6a4f4fb0
Robert Chen <bergintonatcapital@gmail.com>
2024-01-09 14:50:17 -0500

    Create form post new items

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,5 @@
 const Category = require("../models/category");
+const item = require("../models/item");
 const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
@@ -36,10 +37,36 @@ exports.item_create_get = asyncHandler(async (req, res, next) => {
 	});
 });
 
-exports.item_create_post = asyncHandler(async (req, res, next) => {
-	const category_list = await Category.find({}).exec();
-	res.render("item_form", {
-		title: "Create Item",
-		categories: category_list,
-	});
-});
+exports.item_create_post = [
+	// Validate and sanitize fields.
+	body("item_name").trim().notEmpty().withMessage("Item name is required").escape(),
+	body("item_description").trim().notEmpty().withMessage("Item description is required").escape(),
+	body("item_category").trim().notEmpty().withMessage("Item category is required").escape(),
+	body("item_price").trim().notEmpty().withMessage("Item price is required").isNumeric().withMessage("Item price must be a number").escape(),
+	body("item_num_in_stock").trim().notEmpty().withMessage("Number in stock is required").isInt({ min: 1 }).withMessage("Number in stock must be greater than zero").escape(),
+
+	asyncHandler(async (req, res, next) => {
+		const errors = validationResult(req);
+		console.log(req.body.item_category)
+		const item = new Item({
+			name: req.body.item_name,
+			description: req.body.item_description,
+			category: req.body.item_category,
+			price: req.body.item_price,
+			number_in_stock: req.body.item_num_in_stock,
+		});
+
+
+		if (!errors.isEmpty()) {
+			const category_list = await Category.find({}).exec();
+			res.render("item_form", {
+				title: "Create Item",
+				item: item,
+				categories: category_list,
+			});
+		} else {
+			await item.save();
+			res.redirect(item.url);
+		}
+	}),
+];
