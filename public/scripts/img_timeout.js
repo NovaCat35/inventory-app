14546deb05544e58852006dd783386dcdd63a17d
Robert Chen <bergintonatcapital@gmail.com>
2024-01-18 18:48:42 -0500

    Add a mini tv and video

--- public/scripts/img_timeout.js
+++ public/scripts/img_timeout.js
@@ -1,8 +1,11 @@
 // Use JavaScript to hide the internet-gif after 5.3 seconds
 document.addEventListener("DOMContentLoaded", function () {
 	const internetGif = document.getElementById("internet-gif");
+	const catMarchVideo = document.getElementById("background-video");
 
 	setTimeout(function () {
 		internetGif.style.display = "none";
+		catMarchVideo.play();
 	}, 5300);
 });
+
